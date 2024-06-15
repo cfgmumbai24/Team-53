260d8f848fb0f895de94464670028ddb12f009cd
Sajal3295 <sajal01ynr@gmail.com>
2024-06-15 22:43:28 +0530

    fellow and student schema

---
+++ backend/src/models/fellowmodel.js
@@ -0,0 +1,33 @@
+import mongoose,{Schema} from "mongoose";
+
+const fellowSchema = new Schema(
+    {
+        fellowName: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+        avatar: {
+            type: String, // cloudinary url
+            required: true,
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required']
+        },
+        refreshToken: {
+            type: String
+        }
+    }, 
+    {
+        timestamps: true
+    }
+)
+
+export const Fellow = mongoose.model("Fellow", fellowSchema)
