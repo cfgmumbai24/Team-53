8913c74c1cd0fd61eb969bc1c5b40071895a65be
Sajal3295 <sajal01ynr@gmail.com>
2024-06-15 23:56:10 +0530

    models added of assessment and admin

---
+++ backend/src/models/assesment.model.js
@@ -0,0 +1,49 @@
+import mongoose,{Schema} from "mongoose";
+
+const assessmentSchema = new Schema(
+    {
+        fellow: {
+            type: Schema.Types.ObjectId,
+            ref: "Fellow",
+            required: true
+        },
+        student: {
+            type: Schema.Types.ObjectId,
+            ref: "Student",
+            required: true
+        },
+        // assessmentDate: {
+        //     type: Date,
+        //     required: true
+        // },
+        listening: {
+            type: Number,
+            default: 0
+        },
+        numeracy: {
+            type: Number,
+            default: 0
+        },
+        speaking: {
+            type: Number,
+            default: 0
+        },
+        literacy: {
+            type: Number,
+            default: 0
+        },
+        reading: {
+            type: Number,
+            default: 0
+        },
+        total: {
+            type: Number,
+            default: 0
+        }
+    },
+    {
+        timestamps: true
+    }
+)
+
+export const Assessment = mongoose.model("Assessment", assessmentSchema)
