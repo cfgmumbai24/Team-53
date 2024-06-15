ec2ecadc8ba12e05e2f4e99f4ce548a4584eafb6
SRP4214 <shiv160804@gmail.com>
2024-06-16 05:02:27 +0530

    bug fix

--- backend/src/controllers/admin.controllers.js
+++ backend/src/controllers/admin.controllers.js
@@ -1,7 +1,7 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { ApiError } from "../utils/ApiError.js";
 import { Admin } from "../models/admin.model.js";
-import { Fellow } from "../models/admin.model.js";
+import { Fellow } from "../models/fellow.model.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import jwt from "jsonwebtoken";
 import mongoose from "mongoose";
